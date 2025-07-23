e6caaf39f6914f25c0cc38dbcc70ad2de35c811f
Luis Gustavo | Touka <gustavoluis.pixell@outlook.com>
2025-07-23 17:49:49 -0300

    feat: executa BookCli na inicialização
    - Injeta `BookCli`
    - Chama `cli.start()` dentro do `CommandLineRunner` para habilitar a interface de linha de comando

--- src/main/java/com/touka/book/BookApplication.java
+++ src/main/java/com/touka/book/BookApplication.java
@@ -1,13 +1,25 @@
 package com.touka.book;
 
+import com.touka.book.infrastructure.cli.BookCli;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookApplication {
+public class BookApplication implements CommandLineRunner {
+
+	private final BookCli cli;
+
+	public BookApplication(BookCli cli) {
+		this.cli = cli;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+		cli.start();
+	}
 }
